dc37f001f618537c489d4ce73e7d852ca06aab03
Mattiemateo <130217368+Mattiemateo@users.noreply.github.com>
2026-02-16 19:46:37 +0100

    find goto

--- minecraft_mcp.py
+++ minecraft_mcp.py
@@ -15,25 +15,61 @@ mineflayer = require('mineflayer')
 pathfinder = require('mineflayer-pathfinder')
 
 mcData = None
+global movements
 
-MakeBot("chatter")
+MakeBot("chopper")
 
 message_list = ["this is a test message"]
 
 mcp = FastMCP(name="Minecraft chat helper")
-
-
+bot.loadPlugin(pathfinder.pathfinder)
 
 @On(bot, 'spawn')
 def handle(*args):
-	print("I spawned 👋")
+    print("I spawned 👋")
+    
+    global mcData
+    print("I spawned 👋")
+    
+    # Load minecraft data for block IDs
+    mcData = require('minecraft-data')(bot.version)
+    
+    bot.waitForChunksToLoad()
+    print("Chunks loaded!")
 
-'''@On(bot, 'message')
-def handleGlobalMessage(jsonMsg, position, sender, verified, *args):
-    print(jsonMsg)
-    message_list.append(jsonMsg)
-    print(jsonMsg)
-'''
+    movements = pathfinder.Movements(bot)
+    
+    @On(bot, 'chat')
+    def handleMsg(this, sender, message, *args):
+        message_list.append(message)
+        if len(message_list) > 5:
+            _ = message_list.pop(0)
+
+        print ("got", message, "from", sender)
+
+        if sender != bot.username and sender:
+            if "come here" in message:
+                movements = pathfinder.Movements(bot)
+                player = bot.players[sender]
+                target = player.entity
+                pos = target.position
+                bot.pathfinder.setMovements(movements)
+                try :
+                    bot.pathfinder.setGoal(pathfinder.goals.GoalNear(pos.x, pos.y, pos.z, 1))
+                except:
+                    print("fuckkkkkk")
+                    bot.chat("fuckkkkk")
+            elif 'find tree' in message:
+                find_block('oak_log')
+            elif 'find dirt' in message:
+                find_block('dirt')
+            elif 'find stone' in message:
+                find_block('stone')
+            elif 'goto tree' in message:
+                find_block('stone')
+
+
+
 def find_block(block_name):
     global mcData
     # Load minecraft data for block IDs
@@ -69,19 +105,22 @@ def find_block(block_name):
         
         pos = positions[0]
         bot.chat(f"Found {block_name} at {pos.x}, {pos.y}, {pos.z}!")
+        goto(pos.x, pos.y, pos.z)
         return bot.blockAt(pos)
     else:
         print(f"No {block_name} found")
         bot.chat(f"No {block_name} found!")
         return None
 
-@On(bot, 'chat')
-def handleMsg(this, sender, message, *args):
-    message_list.append(message)
-    if len(message_list) > 5:
-        _ = message_list.pop(0)
-    
-    print ("got", message, "from", sender)
+def goto(x, y, z):
+    movements = pathfinder.Movements(bot)
+    bot.pathfinder.setMovements(movements)
+    try :
+        bot.pathfinder.setGoal(pathfinder.goals.GoalNear(x, y, z, 2))
+    except:
+        print("fuckkkkkk")
+        bot.chat("fuckkkkk")
+
 
 @On(bot, 'end')
 def OnEnd(reason, *args):
